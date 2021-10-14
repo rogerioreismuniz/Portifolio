1323d80e2b73d506cff804074aa8118b2b0ded6a
rogerioreismuniz <rmuniz.dev@gmail.com>
2021-10-14 15:03:07 -0300

    Alert

--- script.js
+++ script.js
@@ -4,21 +4,6 @@ document.querySelector(".hamburguer").addEventListener("click", () =>
     document.querySelector(".container").classList.toggle("show-menu")
 );
 
-/* ------- Video ------- */
-
-var videoPlayer = document.getElementById("VideoPlayer");
-var myVideo = document.getElementById("myVideo");
-
-function stopVideo() {
-    videoPlayer.style.display = "none";
-    myVideo.src = "none";
-}
-
-function playVideo(file) {
-    myVideo.src = file;
-    videoPlayer.style.display = "block";
-}
-
 /* ------- Loader ------- */
 
 $(window).on('load', function() {
@@ -28,47 +13,7 @@ $(window).on('load', function() {
 })
 
 
-// Função para mostrar os demais vídeos na galeria.
-function mais() {
-
-    var hide = document.getElementById("hide-list");
-    var show = document.getElementById("show-list");
-
-    if (show.style.display == "" || show.style.display == "none") {
-
-        hide.style.display = "none";
-        show.style.display = "block";
-        document.getElementById("show-list").animate([
-            // keyframes
-            {
-                transform: 'scale(0.9)',
-                opacity: '0.2'
-            }, {
-                transform: 'scale(1)'
-            }
-        ], {
-            // timing options
-            duration: 700,
-
-        });
-
-
-    } else if (hide.style.display == "" || hide.style.display == "none") {
-        show.style.display = "none";
-        hide.style.display = "block";
-        document.getElementById("hide-list").animate([
-            // keyframes
-            {
-                transform: 'scale(0.9)',
-                opacity: '0.2'
-            }, {
-                transform: 'scale(1)'
-            }
-        ], {
-            // timing options
-            duration: 700,
-
-        });
-
-    }
+/* ------- Função alerta ------- */
+function Alerta(){
+    alert("Mensagem enviada!")
 }
